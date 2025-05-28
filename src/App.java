c1cf68022e2b0ef36acd303f5250ecd1883da3ad
Zhukov Aleksandr <a.zhukov@uzum.com>
2025-05-28 19:21:14 +0200

    owaijofiw

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Привет мир!!!");
+        int a = 2;
+        if (a == 2){
+            System.out.println("Привет мир!!!");
+        }
     }
 }
 
